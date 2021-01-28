df5c677e72325bad4436a00378ec4e271fa853eb
Pauline Woo <pwoo@ucsd.edu>
2021-01-28 10:34:20 -0800

    first commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
 var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
@@ -20,14 +21,14 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('IxD secret key'));
-app.use(express.session());
-app.use(app.router);
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.methodOverride());
+// app.use(express.cookieParser('IxD secret key'));
+// app.use(express.session());
+// app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
+app.get("/project/:name", project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
--- routes/hello.js
+++ routes/hello.js
@@ -7,4 +7,8 @@ exports.view = function(req, res){
   res.render('index', {
   	'name': '',
   });
+  // var nameToShow = req.params.userName;
+  // res.render("hello", {
+  //   "name": nameToShow
+  // });
 };
